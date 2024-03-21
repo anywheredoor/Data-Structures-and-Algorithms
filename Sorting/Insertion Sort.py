4cb90e18eaad5bf64561754210c53fa240c6844a
mini-dora <121270060+mini-dora@users.noreply.github.com>
2024-03-21 20:30:11 +0900

    Create Insertion Sort.py

---
+++ Sorting/Insertion Sort.py
@@ -0,0 +1,10 @@
+array = [6, 9, 1, 2, 3, 8, 4, 5, 7, 0]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j-1]:
+            array[j], array[j-1] = array[j-1], array[j]
+        else:
+            break
+
+print(array)
