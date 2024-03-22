55e65071106e7e18c3552eec52f80f316c413beb
mini-dora <121270060+mini-dora@users.noreply.github.com>
2024-03-22 10:20:14 +0900

    Create Quick Sort Pythonic.py

---
+++ Sorting/Quick Sort Pythonic.py
@@ -0,0 +1,17 @@
+array = [9, 6, 1, 2, 3, 8, 4, 5, 7, 0]
+
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+
+print(quick_sort(array))
