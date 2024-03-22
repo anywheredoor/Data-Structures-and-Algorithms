4f5cbf4f34985973ae52cf71177dba262d733d4e
mini-dora <121270060+mini-dora@users.noreply.github.com>
2024-03-22 10:09:34 +0900

    Create Quick Sort Hoare Partition.py

---
+++ Sorting/Quick Sort Hoare Partition.py
@@ -0,0 +1,29 @@
+array = [9, 6, 1, 2, 3, 8, 4, 5, 7, 0]
+
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    
+    pivot = start
+    left = start + 1
+    right = end
+
+    while left <= right:
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+
+        if left > right:
+            array[pivot], array[right] = array[right], array[pivot]
+        else:
+            array[left], array[right] = array[right], array[left]
+
+    quick_sort(array, start, right-1)
+    quick_sort(array, right+1, end)
+
+
+quick_sort(array, 0, len(array)-1)
+
+print(array)
