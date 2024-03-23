52a648a6255289662d50542d864b0e3e119a4e6f
mini-dora <121270060+mini-dora@users.noreply.github.com>
2024-03-23 11:39:43 +0900

    Create Min Heap.py

---
+++ Heap/Min Heap.py
@@ -0,0 +1,17 @@
+import heapq
+
+def heapsort(iterable):
+    h = []
+    result = []
+
+    for value in iterable:
+        heapq.heappush(h, value)
+
+    for _ in range(len(h)):
+        result.append(heapq.heappop(h))
+
+    return result
+
+result = heapsort([1, 9, 0, 2, 5, 3, 4, 7, 8, 6])
+
+print(result)
